ea2e58c566ee9f6b4b0bdbdb0425f43b595c1442
Rodrigo Sandeski <sandeski47@gmail.com>
2022-05-04 03:46:35 -0300

    FInished

--- lambda6-csharp/Program.cs
+++ lambda6-csharp/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using lambda6_csharp.Entities;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,38 @@ namespace lambda6_csharp
     {
         static void Main(string[] args)
         {
+            List<Product> products = new List<Product>();
+            string path = @"c:\temp\in.txt";
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        var aux = sr.ReadLine().ToString().Split(',');
+                        string name = aux[0];
+                        double price = double.Parse(aux[1],CultureInfo.InvariantCulture);
+                        
+                        products.Add(new Product(name, price));
+                    }
+                    var averagePrice = products.Average(p => p.Price);
+
+                    Console.WriteLine(averagePrice.ToString("F2",CultureInfo.InvariantCulture));
+                    var productsLowerPrice = products.Where(p => p.Price < averagePrice).Select(p => p);
+                    var productsOrdened = productsLowerPrice.OrderByDescending(p => p.Name).Select(p => p.Name);
+                    foreach (var p in productsOrdened)
+                    {
+                        Console.WriteLine(p);
+                    }
+
+                }
+
+            }catch(Exception ex){
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+            Console.ReadKey();
+            
         }
     }
 }
